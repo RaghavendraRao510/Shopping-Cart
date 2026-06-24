90539760b695a83709e5b4e14d6610b1f3c9cd2b
RAGHAVENDRA RAO <nanuvalaraghavendrarao7@gmail.com>
2026-06-24 09:35:54 +0530

    Add files via upload

---
+++ Shopping Cart.py
@@ -0,0 +1,7 @@
+prices = [10.0, 25.5, 4.99, 100.0, 50.0]
+total_cost = 0
+for price in prices:
+    total_cost += price
+    print(f"Item cost: {price}")
+print(f"Total Shopping cart cost: {total_cost}")
+
